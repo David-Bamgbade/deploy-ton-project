ac853865b205703378b3c87702e026f87d9e736d
David-Bamgbade <davidbamgbade082@gmail.com>
2025-04-03 01:45:01 +0100

    vercel.json update

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,3 @@
-// import { TonConnectUIProvider } from '@tonconnect/ui-react';
-// import { Header } from './Header';
-// import './index.css';
-// import {WalletConnector} from "./components/WalletConnector.tsx";
-//
-// export function App() {
-//
-//
-//     return (
-//         <TonConnectUIProvider manifestUrl="https://liquidtoken.vercel.app/manifest.json">
-//             <div className="app-container">
-//                 <Header />
-//                 <main className="landing">
-//                     <section className="hero">
-//                         <h1 className="hero-title">Unlock the Future of Crypto</h1>
-//                         <p className="hero-subtitle"></p>
-//                             You buy the top 10 cryptocurrencies at once when you mint new tokens
-//                             <p>
-//                                 mint, withdraw, transfer and claim airdrops with seamless TON integration.
-//                             </p>
-//
-//                         <WalletConnector/>
-//                     </section>
-//                 </main>
-//             </div>
-//         </TonConnectUIProvider>
-//     );
-// }
-//
-// export default App;
-
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './Header';
